02ad4b142a708692b65b76f2989cc27933b17589
Roger <roger.neil.davenport@gmail.com>
2018-11-14 12:51:44 -0800

    fileModel no methods

--- model/fileModel.js
+++ model/fileModel.js
@@ -4,8 +4,16 @@
 const mongoose = require('mongoose');
 
 const File = mongoose.Schema({
+  userId: {type: mongoose.Schema.Types.ObjectId, required: true, ref: 'userModel'},
   name : {type: String, required: true },
-  dosage : {type: Number, required: true },
+  description: {type: String, required: false },
+  executeType: {type: String, required: true },
+  executeDate: Date,
+  cycle: { type: Number, required: true },
+  email: { type: [{type: mongoose.Schema.Types.ObjectId, required: true, ref: 'userModel'}], required: false},
+  s3Location: { type: String, required: false},
+  canView: { type: [{type: mongoose.Schema.Types.ObjectId, required: true, ref: 'userModel'}], required: false},
+
 }, {timestamps: true});
 
 module.exports = mongoose.model('files', File);
