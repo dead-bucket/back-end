5d6cb184a8a99282d01c7fae52b37efc7e4bba47
Roger <roger.neil.davenport@gmail.com>
2018-11-15 08:14:30 -0800

    methods for execute date on filemodel

--- model/fileModel.js
+++ model/fileModel.js
@@ -16,4 +16,23 @@ const File = mongoose.Schema({
 
 }, {timestamps: true});
 
+File.methods.createExecuteDate = function () {
+  return new Promise((resolve, reject) => { 
+    if(!this.cycle) return reject(new Error('Authorization failed in fileModel.js no cycle provided'));
+    let milliseconds = Date.now();
+    milliseconds =  milliseconds + (this.cycle*24*60*60*1000);
+    this.excecuteDate = new Date(milliseconds);
+    resolve(this);
+  });
+};
+
+File.methods.updateExecuteDate = function(lastLogin) {
+  return new Promise((resolve, reject) => {
+    if(!lastLogin) return reject (new Error('Authorization falied in fileModel.js no last login provided'));
+    let milliseconds = new Date(lastLogin);
+    milliseconds =  milliseconds + (this.cycle*24*60*60*1000);
+    this.executeDate = new Date(milliseconds);
+    resolve(this);
+  });
+};
 module.exports = mongoose.model('files', File);
