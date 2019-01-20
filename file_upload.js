dcdc578e7974dbfc55e1e6646a160dc467905d80
Roger <roger.neil.davenport@gmail.com>
2019-01-20 12:18:50 -0800

    tidied up file upload js module

--- file_upload.js
+++ file_upload.js
@@ -1,11 +1,7 @@
-const fs = require('fs');
 const AWS = require('aws-sdk');
-require('dotenv').config();
-console.log('accesskey  ',process.env.ACCESS_KEY);
 
 module.exports = (picture, id) => {
   
-  console.log('in upload this is what we get', picture);
   let buf = new Buffer(picture.replace(/^data:image\/\w+;base64,/, ''),'base64');
   const s3 = new AWS.S3({
     accessKeyId: process.env.ACCESS_KEY,
@@ -15,19 +11,16 @@ module.exports = (picture, id) => {
   const params = {
     ACL: 'public-read',
     Bucket: 'thoughtline', // pass your bucket name
-    Key: `${id}.jpeg`, // file will be saved as testBucket/contacts.csv
+    Key: `${id}.jpeg`, // file will be saved as the file name will be the user _id.jpeg
     Body: buf,
     ContentEncoding: 'base64',
     ContentType: 'image/jpeg',
   };
+
   return new Promise ((resolve, reject) => {
     s3.upload(params, (err, data) => err ? reject(err) : resolve(data));
   });
-  // s3.upload(params, function(s3Err, data) {
-  //   if (s3Err) throw s3Err;
-  //   console.log(`File uploaded successfully at ${data.Location}`);
-  //   return data.Location;
-  // });
+  
 
 };
 
