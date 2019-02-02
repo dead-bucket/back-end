e4cee2c71842dcafaed40fb9fb20fc6687c7501d
Roger <roger.neil.davenport@gmail.com>
2019-02-01 17:11:16 -0800

    some code cleanup

--- route/send-invite.js
+++ route/send-invite.js
@@ -5,7 +5,6 @@ const errorHandler = require('../lib/error-handler');
 const sendEmail = require('../lib/send-email');
 
 
-// const User = require('../model/userModel');
 const bearerAuth = require('../lib/bearer-auth-middleware');
 function validateEmail(email) {
   var re = /\S+@\S+\.\S+/;
@@ -18,10 +17,8 @@ module.exports = router => {
     
     .post(bearerAuth, bodyParser, (req, res) => {
 
-      // if(!validateEmail(req.body.email)) return Promise.reject(new Error('Validation error'));
       if(req.body.email) {
         if(!validateEmail(req.body.email)) return errorHandler(new Error('validation failed, not a valid email format'), res);
-        // console.log('user search email', req.body.email);
         return sendEmail(req)
           .then(data => {
             res.status(200).json(data);
--- route/user-search.js
+++ route/user-search.js
@@ -16,24 +16,18 @@ module.exports = router => {
   router.route('/usersearch/:email?')
     
     .get(bearerAuth, bodyParser, (req, res) => {
-      console.log('usersearch', req.query.email);
       
-      // if(!validateEmail(req.body.email)) return Promise.reject(new Error('Validation error'));
       if(req.query.email) {
         if(!validateEmail(req.query.email)) return errorHandler(new Error('validation failed, not a valid email format'), res);
         // console.log('user search email', req.body.email);
         
         return User.find({email: req.query.email})
           .then(entry => {
-            console.log('entry from search',entry);
             if (entry.length > 0) {
-              console.log('user found');
               res.status(200).json(entry);
             } else {
-              console.log('no user found');
               res.status(404).json(`no user with email ${req.query.email} exists`);
             }
-
           })
           .catch(err => errorHandler(err, res));
       }
