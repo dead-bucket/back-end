2b71be7bf23d423e23248591ba995dc05d9864c5
Denis Molloy <denis.molloy@gmail.com>
2019-02-10 16:37:30 -0800

    swithed inbox route to an email query params

--- route/send-invite.js
+++ route/send-invite.js
@@ -1,34 +1,37 @@
-'use strict';
+"use strict";
 
-const bodyParser = require('body-parser').json();
-const errorHandler = require('../lib/error-handler');
-const sendEmail = require('../lib/send-email');
+const bodyParser = require("body-parser").json();
+const errorHandler = require("../lib/error-handler");
+const sendEmail = require("../lib/send-email");
 
-
-const bearerAuth = require('../lib/bearer-auth-middleware');
+const bearerAuth = require("../lib/bearer-auth-middleware");
 function validateEmail(email) {
   var re = /\S+@\S+\.\S+/;
   return re.test(email);
 }
 
-
 module.exports = router => {
-  router.route('/sendinvite/')
-    
-    .post(bearerAuth, bodyParser, (req, res) => {
+  router
+    .route("/sendinvite/")
 
-      if(req.body.email) {
-        if(!validateEmail(req.body.email)) return errorHandler(new Error('validation failed, not a valid email format'), res);
+    .post(bearerAuth, bodyParser, (req, res) => {
+      console.log("send invite email", req.body.email);
+      if (req.body.email) {
+        if (!validateEmail(req.body.email))
+          return errorHandler(
+            new Error("validation failed, not a valid email format"),
+            res
+          );
         return sendEmail(req)
           .then(data => {
             res.status(200).json(data);
           })
           .catch(err => errorHandler(err, res));
-        
-          
-          
       }
-      if(!req.body.email) return errorHandler(new Error('validation failed, no email specified'), res);
-
+      if (!req.body.email)
+        return errorHandler(
+          new Error("validation failed, no email specified"),
+          res
+        );
     });
 };
