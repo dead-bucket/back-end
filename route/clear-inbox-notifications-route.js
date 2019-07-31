a3cd68ff029c57c71520dca0932f27a160eb0f04
Roger <roger.neil.davenport@gmail.com>
2019-07-31 16:26:05 -0700

    marked as read if viewed in inbox

--- route/clear-inbox-notifications-route.js
+++ route/clear-inbox-notifications-route.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser').json();
 const errorHandler = require('../lib/error-handler');
 const bearerAuth = require('../lib/bearer-auth-middleware');
 const User = require('../model/userModel');
+const Entry = require('../model/entriesModel');
 
 // this will clear the notification flag from the new messages array of the user.
 module.exports = router => {
@@ -11,7 +12,8 @@ module.exports = router => {
     .put(bearerAuth, bodyParser, (req, res) => {
     
       if (req.body.sender) {
-        // console.log('user', req.user);
+        console.log('user', req.user.id);
+        console.log('req.body.sender', req.body.sender);
         if(req.user.newmessages.includes(`${req.body.sender}`)) {
           User.findOne({_id: req.user.id})
             .then(user => {
@@ -19,6 +21,20 @@ module.exports = router => {
               user.newmessages = temp;
               user.save();
               return;
+            })
+            .then(() => {
+              Entry.updateMany({
+                userId: req.body.sender,
+                recipient: req.user.id,
+                delivered: true,
+                read: false,
+                deliverOn: {'$lte': Date.now()},
+              }, {read: true})
+                .then(results => {
+                  console.log('results of entryfind in inboxclearnotifications', results);
+                  return;
+                });
+                
             })
             .then(() => {
               res.sendStatus(204);
